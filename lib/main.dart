3ec38f6839a972cfbfbed937d717e0b41a827295
y.hoshi <ysk.hoshi.1118@gmail.com>
2020-04-07 18:50:17 +0900

    udacity tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/material.dart';
-
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-          color: Colors.greenAccent,
-          height: 400.0,
-          width: 300.0,
-          child: Center(
-              child: Text(
-            'Hello!',
-            style: TextStyle(fontSize: 40.0),
-          ))),
-    );
-  }
-}
+import 'package:flutter_trial/category_route.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-              title: Text(
-            'Hello Rectangle',
-          )),
-          body: HelloRectangle()),
-    ),
-  );
+  runApp(UnitConverterApp());
+}
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Unit Converter',
+        home: Scaffold(
+            backgroundColor: Colors.green[100],
+            body: Center(
+              child: CategoryRoute(),
+            )));
+  }
 }
